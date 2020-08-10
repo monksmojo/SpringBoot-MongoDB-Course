908b0fe1ca173da2156e0f4747421ba2febe7c7a
udit.makkar@tothenew <udit-makkar-ttn>
2020-08-10 18:46:27 +0530

    creating directory structure

--- springBootmongo/src/main/java/com/examples/kris/springBootmongo/entity.java
+++ springBootmongo/src/main/java/com/examples/kris/springBootmongo/entity.java
@@ -1,4 +1,37 @@
 package com.examples.kris.springBootmongo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "Book")
+@Getter
+@Setter
 public class entity {
+
+    @Field(name = "book_id")
+    @Id
+    private Long bookId;
+
+    @Field(name = "book_name")
+    private String bookName;
+
+    private String author;
+
+    public entity(Long bookId, String bookName, String author) {
+        this.bookId = bookId;
+        this.bookName = bookName;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "entity{" +
+                "bookId=" + bookId +
+                ", bookName='" + bookName + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
