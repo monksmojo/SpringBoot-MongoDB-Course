b5b6221c86f67214265aaab381d062309f060501
udit.makkar@tothenew <udit-makkar-ttn>
2020-08-22 20:01:55 +0530

    final draft

--- spring-mongo-embedded/src/main/java/com/javaTechie/springmongoembedded/controller/BuyerController.java
+++ spring-mongo-embedded/src/main/java/com/javaTechie/springmongoembedded/controller/BuyerController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
 import java.util.List;
 
 @RestController
@@ -15,6 +16,7 @@ public class BuyerController {
     @Autowired
     BuyerService buyerService;
 
+    // generate buyers
     @PostMapping(path = "/place-order")
     public ResponseEntity<String> placeOrder(@RequestBody Buyer buyer){
         if(buyerService.placeOrder(buyer)){
@@ -25,14 +27,53 @@ public class BuyerController {
         }
     }
 
+    // get all
     @GetMapping(path = "/get-all-buyers/")
     public ResponseEntity<List<Buyer>> getAllBuyers(){
         List<Buyer> buyerList=buyerService.getAllBuyers();
         return new ResponseEntity<>(buyerList,HttpStatus.OK);
     }
 
+    // get-buyers-by-name
+    @GetMapping(path = "/get-buyer-by-name/{name}")
+    public ResponseEntity<Collection<Buyer>> getBuyersByName(@PathVariable String name){
+        Collection<Buyer> buyerList=buyerService.getBuyersBYName(name);
+        return new ResponseEntity<>(buyerList,HttpStatus.OK);
+    }
 
+    // get-buyer-by-id
+    @GetMapping(path = "/get-buyer-by-id/{buyerId}")
+    public ResponseEntity<Buyer> getBuyerById(@PathVariable Long buyerId){
+        Buyer buyer=buyerService.getBuyerById(buyerId);
+        return new ResponseEntity<>(buyer,HttpStatus.OK);
+    }
 
+    // get-buyer-by-name
+    @GetMapping(path = "/get-buyer-by-address")
+    public ResponseEntity<Collection<Buyer>> getBuyerByAddress(@RequestParam String city,@RequestParam String state){
+        Collection<Buyer> buyerCollection=buyerService.getBuyerByStateAndCity(city,state);
+        return new ResponseEntity<>(buyerCollection,HttpStatus.OK);
+    }
 
+    // get-buyer-by-pin-code
+    @GetMapping(path = "/get-buyer-by-pin-code")
+    public ResponseEntity<Collection<Buyer>> getBuyerByAddress(@RequestParam Integer pinCode){
+        Collection<Buyer> buyerCollection=buyerService.getBuyerByPinCode(pinCode);
+        return new ResponseEntity<>(buyerCollection,HttpStatus.OK);
+    }
+
+    //get-buyer-products
+    @GetMapping(path = "/get-buyer-products/{buyerId}")
+    public ResponseEntity<Collection<Buyer>>getBuyerProduct(@PathVariable Long buyerId){
+        Collection<Buyer> buyerCollection=buyerService.getBuyerProducts(buyerId);
+        return new ResponseEntity<>(buyerCollection,HttpStatus.OK);
+    }
+
+    //filter-products
+    @GetMapping(path = "/filter-products/{price}")
+    public ResponseEntity<Collection<Buyer>>getFilteredProducts(@PathVariable Double price) {
+        Collection<Buyer> buyerCollection = buyerService.getProductsOnBasesOfMaxPrice(price);
+        return new ResponseEntity<>(buyerCollection, HttpStatus.OK);
+    }
 
 }
--- spring-mongo-embedded/src/main/java/com/javaTechie/springmongoembedded/service/BuyerService.java
+++ spring-mongo-embedded/src/main/java/com/javaTechie/springmongoembedded/service/BuyerService.java
@@ -5,10 +5,12 @@ import com.javaTechie.springmongoembedded.repository.BuyerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 
 @Service
 public class BuyerService {
+
     @Autowired
     BuyerRepository buyerRepository;
 
@@ -20,4 +22,35 @@ public class BuyerService {
     public List<Buyer> getAllBuyers() {
         return buyerRepository.findAll();
     }
+
+    public Buyer getBuyerById(Long id){
+        return buyerRepository.findByBuyerId(id);
+    }
+
+    public Collection<Buyer> getBuyersBYName(String name){
+        return buyerRepository.findByNameContaining(name);
+    }
+
+    public Collection<Buyer> getBuyerByStateAndCity(String city,String state){
+        return buyerRepository.findByCityAndState(city,state);
+    }
+
+    public Collection<Buyer> getBuyerByPinCode(Integer pinCode){
+        return buyerRepository.findByPinCode(pinCode);
+    }
+
+    public Collection<Buyer> getBuyerProducts(Long id){
+        return buyerRepository.findBuyersProducts(id);
+    }
+
+    public Collection<Buyer>
+
+
+    getProductsOnBasesOfMaxPrice(Double price){
+        return buyerRepository.findByPriceFiler(price);
+    }
+
+
+
+
 }
